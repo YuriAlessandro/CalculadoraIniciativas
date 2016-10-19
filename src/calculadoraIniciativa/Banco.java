3405d2c918693070434b41409c01081b051f4e86
Elton Viana <eltonvs@outlook.com>
2016-10-19 14:46:30 -0300

    Some improvements on project organization

--- src/calculadoraIniciativa/Banco.java
+++ src/calculadoraIniciativa/Banco.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package calculadorainiciativa;
+package calculadoraIniciativa;
 
 import java.util.ArrayList;
 
--- src/calculadoraIniciativa/CalculadoraIniciativa.java
+++ src/calculadoraIniciativa/CalculadoraIniciativa.java
@@ -1,4 +1,4 @@
-package calculadorainiciativa;
+package calculadoraIniciativa;
 
 /**
  *
